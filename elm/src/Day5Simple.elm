adaf5b070e5b87e2836f709e8156adc6a0736200
Andrey Miskov <amiskov@gmail.com>
2020-12-05 15:53:08 +0300

    Fix note

--- elm/src/Day5Simple.elm
+++ elm/src/Day5Simple.elm
@@ -6,8 +6,8 @@ import Day5.Input exposing (input)
 
 toSeatId : String -> Int
 toSeatId code =
-    -- We don't have to do `row * 8 + col` since this is the shift by 3 digits of the number
-    -- (they will filled after `+ col`):
+    -- We don't have to do `row * 8 + col` since `row * 8` is the same
+    -- as bit-shift by 3 digits (they will filled after `+ col`):
     -- FBFBBFF    → 0101100 → 44
     -- 0101100000 → 352 == 44 * 8
     -- FBFBBFFRLR → 0101100101 → 357 == 44 * 8
